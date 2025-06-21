62d1d882631c119c84cc229577c27fc8ca99a16e
nrjusta12 <njusta@yahoo.com>
2025-06-21 17:57:43 -0500

    Replaced Map Code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,11 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
-const Map = () => {
-  return (
-    <MapContainer center={[39.8283, -98.5795]} zoom={4} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
-  );
-};
-
-export default Map;
+<MapContainer
+  center={[39.8283, -98.5795]} // Geographic center of the contiguous U.S.
+  zoom={4.5}
+  style={{ height: "100vh", width: "100%" }}
+  scrollWheelZoom={true}
+>
+  <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  />
+</MapContainer>
