f8c768e740362484e6e4093b1ed23d029b3b82ed
nrjusta12 <njusta@yahoo.com>
2025-06-21 15:42:38 -0500

    Delete root-level App.tsx (duplicate)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,1 @@
-import React from "react";
 
-function App() {
-  return (
-    <div className="w-full h-full flex items-center justify-center text-xl text-gray-800">
-      Interactive US Map Coming Soon
-    </div>
-  );
-}
-
-export default App;
